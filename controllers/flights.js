fa9f592a5f80cbd3527cc69956fa91efef5f9e96
zuri227 <zuri22780@gmail.com>
2022-02-28 16:39:07 -0500

    Add new flight view

---
+++ controllers/flights.js
@@ -0,0 +1,9 @@
+
+
+function newFlight(req, res) {
+  res.render("flights/new")
+}
+
+export {
+  newFlight as new,
+}
--- routes/flights.js
+++ routes/flights.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import * as flightsCtrl from "../controllers/flights.js"
 
 const router = Router()
 
