b4c96223b82f371aa0951969f8aa2a5e1f4b3206
zuri227 <zuri22780@gmail.com>
2022-02-27 20:04:22 -0500

    Add model

---
+++ models/flight.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+  airline: String,
+  airport: String,
+  flightNo: Number,
+  departs: Date
+})
+
+const Flight = mongoose.model("Flight", flightSchema)
+
+export {
+  Flight
+}
