680dfc034816f60e284ea468f2e4d33c4907ae60
zuri227 <zuri22780@gmail.com>
2022-02-28 21:13:51 -0500

    Add validations to model

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,7 +8,7 @@ function create(req, res) {
   const flight = new Flight(req.body)
   flight.save(function(err){
     if (err) return res.redirect("/flights/new")
-    res.redirect("/flights/new")
+    res.redirect("/flights")
   })
 }
 
--- models/flight.js
+++ models/flight.js
@@ -3,14 +3,28 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const flightSchema = new Schema({
-  airline: String,
-  airport: {type: String, default: "DEN"},
-  flightNo:{
+  airline: {
+    type: String,
+    enum: ["American", "Southwest", "United"]
+  },
+  airport: {
+    type: String, 
+    default: "DEN",
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"]
+  },
+  flightNo: {
     type: Number,
     min: 10,
     max: 9999
   },
-  departs: Date, 
+  departs: {
+    type: Date, 
+    default: function() {
+      return Date.now()+ 365
+    } 
+  },
+}, {
+  timestamps: true
 })
 
 const Flight = mongoose.model("Flight", flightSchema)
